5f780f4a57f002121e630834ff0c0fe5148087af
nf222jd <nf222jd@student.lnu.se>
2025-12-15 15:51:39 +0100

    get all ids from html

---
+++ contact.js
@@ -0,0 +1,29 @@
+// Get id:s from html file
+
+const contactForm = document.getElementById("contactForm");
+
+// Input fields
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+// Error messages
+const firstNameError = document.getElementById("firstNameError");
+const lastNameError = document.getElementById("lastNameError");
+const emailError = document.getElementById("emailError");
+const subjectError = document.getElementById("subjectError");
+const messageError = document.getElementById("messageError");
+
+// Character counter
+const charCounter = document.getElementById("charCounter");
+
+// Reset button
+const resetBtn = document.getElementById("resetBtn");
+
+// Success message
+const successMsg = document.getElementById("successMsg");
+
+//-------------------------------------------------------------------
+
